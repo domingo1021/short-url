752f2512098e1f2246e72db0ae5c04fca9a0cb4f
domingo1021 <domingo.backend@gmail.com>
2024-09-01 18:01:35 +0800

    refactor: refactor main.py with pylint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
+"""
+This module provides the main entry point for the Flask application.
+
+It sets up the Flask app, registers blueprints, and configures the Swagger generator.
+"""
 from flask import Flask
 
 from app.api.url import url_api
 from app.utils.swagger import SwaggerGenerator
 
 def create_app():
+    """
+    Function to create the Flask app, and add all components to it.
+    """
     app = Flask(__name__)
 
     app.register_blueprint(url_api)
@@ -11,4 +19,4 @@ def create_app():
 
     return app
 
-app = create_app()
+main_app = create_app()
