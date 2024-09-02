865598ccde5c934f8ba5d3c3c5814c36528b19ef
domingo1021 <domingo.backend@gmail.com>
2024-09-02 12:22:25 +0800

    feat: add env variable for Redis connection

--- app/utils/redis_client.py
+++ app/utils/redis_client.py
@@ -1,9 +1,15 @@
 """
 Redis client connection model that connects to the Redis server.
 """
+import os
 import redis
+from dotenv import load_dotenv
 
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+load_dotenv()
 
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PORT = os.getenv('REDIS_PORT')
 ORIGIN_URL_PREFIX = 'original:'
 SHORT_URL_PREFIX = 'short:'
+
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
